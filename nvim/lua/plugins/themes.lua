19f3682025d55067bf5ff7d51df9b8defbd54dcd
titomundo <tito.alberto.mundo@gmail.com>
2025-04-07 16:15:35 -0600

    fonts

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,22 +1,35 @@
 return {
-    {
-        "thesimonho/kanagawa-paper.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        "sainnhe/sonokai",
-    },
-    {
-        'eddyekofo94/gruvbox-flat.nvim',
-        priority = 1000,
-        enabled = true,
-    },
-    {
-        "morhetz/gruvbox"
-    },
-    {
-        "navarasu/onedark.nvim",
-    },
+	{
+		"thesimonho/kanagawa-paper.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"sainnhe/sonokai",
+	},
+	{
+		"eddyekofo94/gruvbox-flat.nvim",
+		priority = 1000,
+		enabled = true,
+	},
+	{
+		"morhetz/gruvbox",
+	},
+	{
+		"navarasu/onedark.nvim",
+	},
+	{
+		"mweisshaupt1988/neobeans.vim",
+	},
+	{
+		"nickkadutskyi/jb.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			-- require("jb").setup({transparent = true})
+			vim.cmd("colorscheme jb")
+		end,
+	},
 }
