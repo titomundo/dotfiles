1145073f553676ca6af86ea56d944942a6c60834
Tito Alberto Mundo <tito.alberto.mundo@gmail.com>
2025-04-18 13:39:35 -0600

    gruvbox

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,6 +1,20 @@
 return {
 	{
-	    "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {}
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = true,
+		opts = {},
+	},
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_enable_italic = true
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
 	},
 	{
 		"nickkadutskyi/jb.nvim",
